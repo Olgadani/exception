fd2bd0249fbd4b8fbd3399b7c999cec1cf35265d
Olgadani <olga.metizzz@yandex.ru>
2022-11-13 18:36:43 +0400

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(check("login", "password", "passwrd"));
+    }
 
+    public static boolean check(String login, String password, String confirmPassword) {
+        try {
+            return CheckValues.validate(login, password, confirmPassword);
+        } catch (WrongLoginException e) {
+            System.out.println("Ошибка при проверке логина " + e.getMessage());
+            return false;
+        } catch (WrongPasswordException e) {
+            System.out.println("Ошибка при проверке пароля " + e.getMessage());
+            return false;
+        }
     }
 }
